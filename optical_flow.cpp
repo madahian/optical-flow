d34247fa5ca8e20e073ead659457647c307f2562
Behnam Norouzi <behnam.norouzi@scio.eu>
2022-12-25 20:59:27 +0100

    convert matrix to image

--- optical_flow.cpp
+++ optical_flow.cpp
@@ -6,8 +6,9 @@
 int main() {
 
 	
-	auto loaded_matrix = load_image<int>("C:/AdPT-optical_flow/img.bmp");
-	std::cout << loaded_matrix.tensor() << std::endl;
+	auto loaded_matrix = load_image<int>("C:/AdPT-optical_flow/test_images/rect_right_100x100_0.bmp");
+	//std::cout << loaded_matrix.tensor() << std::endl;
+	save_image(loaded_matrix,"new_image2.bmp");
 	
 	//return 0;
 }
