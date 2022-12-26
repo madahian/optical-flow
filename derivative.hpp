2f654074853be93984c0fd6cbb9bc6d7c1bdd937
Mohammad Madahian <mohammad.madahian@fau.de>
2022-12-26 21:43:43 +0100

    fix: improvment and optimizations for Derivative class

--- derivative.hpp
+++ derivative.hpp
@@ -2,35 +2,34 @@
 
 class Derivative {
     public:
-        Derivative( Matrix<float>& img1, Matrix<float>& img2 );
-        float& Ix(size_t i, size_t j);
-        float& Iy(size_t i, size_t j);
-        float& It(size_t i, size_t j);
+        Derivative(const Matrix<float>& img1, const Matrix<float>& img2 );
+        [[nodiscard]] float& Ix(size_t i, size_t j) const;
+        [[nodiscard]] float& Iy(size_t i, size_t j) const;
+        [[nodiscard]] float& It(size_t i, size_t j) const;
         
     private:
         Matrix<float> img1;
         Matrix<float> img2;
-        float h;
+        static constexpr float h = 0.25;
 };
 
-Derivative::Derivative( Matrix<float>& img1, Matrix<float>& img2 ) {
+Derivative::Derivative(const Matrix<float>& img1, const Matrix<float>& img2 ) {
     this->img1 = img1;
     this->img2 = img2;
-    this->h = 1/4;
 }
 
 
-float& Derivative::Ix(size_t i, size_t j) {
+[[nodiscard]] float& Derivative::Ix(size_t i, size_t j) const {
     float Ix = (h * (img1(i, j+1) - img1(i, j) + img1(i+1, j+1) - img1(i+1, j) + img2(i, j+1) - img2(i, j) + img2(i+1, j+1) - img2(i+1, j)));
     return Ix;
 }
 
-float& Derivative::Iy(size_t i, size_t j) {
+[[nodiscard]] float& Derivative::Iy(size_t i, size_t j) const {
     float Iy = (h * (img1(i+1, j) - img1(i, j) + img1(i+1, j+1) - img1(i, j+1) + img2(i+1, j) - img2(i, j) + img2(i+1, j+1) - img2(i, j+1)));
     return Iy;
 }
 
-float& Derivative::It(size_t i, size_t j) {
+[[nodiscard]] float& Derivative::It(size_t i, size_t j) const {
     float It = (h * (img2(i, j) - img1(i, j) + img2(i, j+1) - img1(i, j+1) + img2(i+1, j) - img1(i+1, j) + img2(i+1, j+1) - img1(i+1, j+1)));
     return It;
 }
