f6b2b71f666b07bf8a22ff60b89577d47819d8e6
Mohammad Madahian <mohammad.madahian@fau.de>
2022-12-29 21:36:42 +0100

    fix: bug fixes for Derivative class.

--- derivative.hpp
+++ derivative.hpp
@@ -3,14 +3,12 @@
 class Derivative {
     public:
         Derivative(const Matrix<float>& img1, const Matrix<float>& img2 );
-        void calculateDerivatives();
-        Matrix<float>& getIx();
-        Matrix<float>& getIy();
-        Matrix<float>& getIt();   
+        float getIx(size_t i, size_t j) const;
+        float getIy(size_t i, size_t j) const;
+        float getIt(size_t i, size_t j) const; 
     private:
         Matrix<float> img1;
         Matrix<float> img2;
-        Matrix<float> Ix, Iy, It {img1.rows(),img1.cols()};
         static constexpr float h = 0.25;
 };
 
@@ -18,31 +16,33 @@ Derivative::Derivative(const Matrix<float>& img1, const Matrix<float>& img2 ) {
     this->img1 = img1;
     this->img2 = img2;
 }
-Matrix<float>& Derivative::getIx() {
+
+float Derivative::getIx(size_t i, size_t j) const {
+    float Ix = 0;
+    if(i+1 >= img1.rows() || j+1 >= img1.cols()) {
+        Ix = (h * (0 - img1(i, j) + 0 - 0 + 0 - img2(i, j) + 0 - 0));
+    }else {
+        Ix = (h * (img1(i, j+1) - img1(i, j) + img1(i+1, j+1) - img1(i+1, j) + img2(i, j+1) - img2(i, j) + img2(i+1, j+1) - img2(i+1, j)));
+    }
     return Ix;
 }
-Matrix<float>& Derivative::getIy() {
+
+float Derivative::getIy(size_t i, size_t j) const {
+    float Iy = 0;
+    if(i+1 >= img1.rows() || j+1 >= img1.cols()) {
+        Iy = (h * (0 - img1(i, j) + 0 - 0 + 0 - img2(i, j) + 0 - 0));
+    }else {
+        Iy = (h * (img1(i, j+1) - img1(i, j) + img1(i+1, j+1) - img1(i+1, j) + img2(i, j+1) - img2(i, j) + img2(i+1, j+1) - img2(i+1, j)));
+    }
     return Iy;
 }
-Matrix<float>& Derivative::getIt() {
+
+float Derivative::getIt(size_t i, size_t j) const {
+    float It = 0;
+    if(i+1 >= img1.rows() || j+1 >= img1.cols()) {
+        It = (h * (0 - img1(i, j) + 0 - 0 + 0 - img2(i, j) + 0 - 0));
+    }else {
+        It = (h * (img1(i, j+1) - img1(i, j) + img1(i+1, j+1) - img1(i+1, j) + img2(i, j+1) - img2(i, j) + img2(i+1, j+1) - img2(i+1, j)));
+    }
     return It;
 }
-
-void Derivative::calculateDerivatives() {
-    for(size_t i = 0; i < img1.rows(); ++i) {
-        for(size_t j = 0; j < img1.cols(); ++j) {
-            if(i+1 >= img1.rows() || j+1 >= img1.cols()) {
-                img1(i+1, j+1) = 0;
-                img1(i+1, j) = 0;
-                img1(i, j+1) = 0;
-                img2(i+1, j+1) = 0;
-                img2(i+1, j) = 0;
-                img2(i, j+1) = 0;
-            }
-            Ix(i,j) = (h * (img1(i, j+1) - img1(i, j) + img1(i+1, j+1) - img1(i+1, j) + img2(i, j+1) - img2(i, j) + img2(i+1, j+1) - img2(i+1, j)));
-            Iy(i,j) = (h * (img1(i+1, j) - img1(i, j) + img1(i+1, j+1) - img1(i, j+1) + img2(i+1, j) - img2(i, j) + img2(i+1, j+1) - img2(i, j+1)));
-            It(i,j) = (h * (img2(i, j) - img1(i, j) + img2(i, j+1) - img1(i, j+1) + img2(i+1, j) - img1(i+1, j) + img2(i+1, j+1) - img1(i+1, j+1)));
-        }
-    }
-}
-
