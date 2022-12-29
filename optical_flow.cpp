2aaf742df8b9966e3a82eb359ae3eec088121c3b
Mohammad Madahian <mohammad.madahian@fau.de>
2022-12-29 21:44:03 +0100

    fix: small changes in optical_flow.cpp

--- optical_flow.cpp
+++ optical_flow.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include "load_image.hpp"
 #include "tensor.hpp"
+#include "derivative.hpp"
 //template< typename ComponentType >
 
 int main() {
+	auto loaded_matrix = load_image<float>("../test_images/rect_right_100x100_0.bmp");
+	auto loaded_matrix2 = load_image<float>("../test_images/rect_right_100x100_1.bmp");
+	
+	/* Example for using Derivative class:
+	Matrix<float> Ix{loaded_matrix.rows(), loaded_matrix.cols()};
+	Derivative d1(loaded_matrix, loaded_matrix2);
+	for(size_t i = 0; i < loaded_matrix.rows(); ++i) {
+		for(size_t j = 0; j < loaded_matrix.rows(); ++j) {
+			Ix(i,j) = d1.getIx(i,j);
+		}
+	}
+	std::cout<<"Ix: " << Ix.tensor() << std::endl; 
+	*/
 
-	
-	auto loaded_matrix = load_image<int>("C:/AdPT-optical_flow/test_images/rect_right_100x100_0.bmp");
-	//std::cout << loaded_matrix.tensor() << std::endl;
 	save_image(loaded_matrix,"new_image2.bmp");
-	
 	//return 0;
 }
